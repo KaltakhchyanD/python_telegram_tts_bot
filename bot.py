e312b04f9df11558d006ad54ade1fd698e2e59c6
DanielK <rafikrafikrafik@yandex.ru>
2019-03-25 14:59:22 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+import sentry_sdk
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+from handlers import start_handler
+from settings import PROXY
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO
+#                    filename='bot.log'
+                    )
+
+logger = logging.getLogger(__name__)
+logger.info('Telegram_tts_bot')
+
+sentry_sdk.init("https://aaa48c902161413c80612b49c2b7897b@sentry.io/1423053")
+
+
+def main():
+    tts_bot = Updater(os.getenv('API_KEY'), request_kwargs = PROXY)
+    dp = tts_bot.dispatcher
+    dp.add_handler(CommandHandler('start', start_handler, pass_user_data=True))
+
+    tts_bot.start_polling()
+    tts_bot.idle()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ handlers.py
@@ -0,0 +1,5 @@
+
+def start_handler(bot, update, user_data):
+    text = 'Hi! This is Text2Speach test bot'
+    update.message.reply_text(text)
+
