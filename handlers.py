14854f6fbb960d549f1335b8863aefdb601be8e7
DanielK <rafikrafikrafik@yandex.ru>
2019-03-26 15:16:58 +0300

    Change text handler - now it sends result of TTS of input text, not just .upper() of it

--- handlers.py
+++ handlers.py
@@ -1,3 +1,5 @@
+from yandex_tts import generate_speach
+
 
 def start_handler(bot, update, user_data):
     text = 'Hi! This is Text2Speach test bot'
@@ -6,4 +8,16 @@ def start_handler(bot, update, user_data):
 
 def text_handler(bot, update, user_data):
     user_text = update.message.text
-    update.message.reply_text(user_text.upper())
+    generate_speach(user_text)
+    send_audio(bot, update, user_data)
+
+
+def send_audio(bot, update, user_data):
+    with open('speech_new.wav', 'rb') as f:
+        bot.send_audio(chat_id=update.message.chat_id, audio=f)
+
+
+def incomming_audio_handler(bot, update, user_data):
+    update.message.reply_text('Got your audio!')
+
+
