ab268802e43ef1337b58e72bb1be0a61a1adb72d
DanielK <rafikrafikrafik@yandex.ru>
2019-06-02 16:24:07 +0300

    Refactor check_wav_length module

--- check_wav_length.py
+++ check_wav_length.py
@@ -2,7 +2,7 @@
 This module provides ability to split wav audio file into several files of size <1K.
 
 It contains these functions to export:
-do_the_thing() - takes source wav file, returns list of smaller wav files of size <1K.
+split_into_files_less_than_1k() - takes source wav file, returns list of smaller wav files of size <1K.
 """
 
 
@@ -13,14 +13,19 @@ import subprocess
 
 def get_duration(filename):
     """Get duration by divising number of frames to framerate with wave."""
-    with wave.open(filename) as wav_f:
-        frames = wav_f.getnframes()
-        rate = wav_f.getframerate()
-        duration = frames / float(rate)
+    # this is hack i think, should revisit later
+    duration = 0
+    if os.path.getsize(filename):
+        with wave.open(filename) as wav_f:
+            frames = wav_f.getnframes()
+            rate = wav_f.getframerate()
+            duration = frames / float(rate)
     return duration
 
 
-# not_empty_wav_files = [i for i in sorted(files_and_dirs_in_cwd) if '.wav' in i and get_duration(i)>=0.5 and i !=  'new_test_voice.wav'    and i != 'text_from_speach.wav' and i != 'speech_test.wav']
+def get_size_in_kb(filename):
+    """Get size of file in KB."""
+    return os.path.getsize(filename) / 1024
 
 
 def convert_from_ogg_to_wav(ogg_audio_file):
@@ -34,8 +39,12 @@ def convert_from_ogg_to_wav(ogg_audio_file):
     return wav_from_ogg
 
 
-def generate_small_files(source_wav):
-    """Split wav audio on silence(silence is 0.1 sec of 2% of max volume(of sample?))."""
+def split_wav_by_silence(source_wav):
+    """
+    Split wav audio on silence(silence is 0.1 sec of 2% of max volume(of sample?)).
+    Return list of non-empty small files
+    """
+
     subprocess.call(
         [
             "sox",
@@ -52,57 +61,49 @@ def generate_small_files(source_wav):
             "restart",
         ]
     )
-    # subprocess.call(['sox', 'text_from_speach.wav', 'new_small_file.wav', 'silence', '0', '1', '0.2', '2%', ':', 'newfile', ':', 'restart'])
+    not_empty_wav_files = find_not_empty_files("new_small_file")
+    return not_empty_wav_files
 
 
-def prepare_some_files(source_wav):
+def find_not_empty_files(name_pattern):
     """
-    Pick nonempty wav files, create new wav file and return all that.???
+    Return list of non-empty wav files in dir
 
     Get files if current dir
     Get all wav files with certain name with duration>0.5s ??? names of result of splitting by silence
-    Create new wav file ???
-    Return list of not empty wav files and new one ???
+    Return list of not empty wav files
     """
 
     files_and_dirs_in_cwd = os.listdir(path=os.getcwd())
     not_empty_wav_files = [
         i
         for i in sorted(files_and_dirs_in_cwd)
-        if ".wav" in i and get_duration(i) >= 0.5 and "new_small_file" in i
+        if ".wav" in i and get_duration(i) >= 0.5 and name_pattern in i
     ]
+
     print(f"NOT EMPTY FILES {not_empty_wav_files}")
-    list_of_names = ["generated_audio_file.wav"]
-
-    with open(list_of_names[0], "wb") as dst_wav_file:
-        pass
-
-    return (not_empty_wav_files, list_of_names)
-
-
-def get_file_name(list_of_names):
-    """Return last name from list of names. ???"""
-    return list_of_names[-1]
+    return not_empty_wav_files
 
 
 def create_new_file_name(list_of_names):
-    """Create and append new filename to list of names by adding _1 to the last filename."""
-    last_name = get_file_name(list_of_names)
+    """
+    Create and append new filename to list of names by adding _1 to the last filename.
+    Return new filename
+    """
+
+    last_name = list_of_names[-1]
     new_name = last_name.split(".")[0] + "_1" + ".wav"
     with open(new_name, "w") as f:
         pass
     list_of_names.append(new_name)
-
-
-def get_size(filename):
-    """Get size of file in KB."""
-    return os.path.getsize(filename) / 1024
+    return new_name
 
 
 def delete_old_files():
     """Delete wav files that was created by split by silence and temp files. ???"""
     files_and_dirs_in_cwd = os.listdir(path=os.getcwd())
     not_empty_wav_files = [i for i in sorted(files_and_dirs_in_cwd) if ".wav" in i]
+    print("Cleaning up")
     print(f"{len(not_empty_wav_files)} files to delete")
     for filename in not_empty_wav_files:
         if "new_small_file" in filename or "generated_audio_file" in filename:
@@ -110,7 +111,7 @@ def delete_old_files():
             os.remove(filename)
 
 
-def do_the_thing(source_wav):
+def split_into_files_less_than_1k(source_wav):
     """
     Split source wav file into files of size <1K.
 
@@ -119,48 +120,46 @@ def do_the_thing(source_wav):
     Add up small files into files of size <1K
     Return list of filenames(that are <1K)
     """
-
-    print("Cleaning up")
+    # 1
     delete_old_files()
-    if get_size(source_wav) < 1024:
-        return [source_wav]
-
-    print("Splitting to small files")
-    generate_small_files(source_wav)
-
-    not_empty_wav_files, list_of_names = prepare_some_files(source_wav)
-
-    print(f"Start size {get_size(get_file_name(list_of_names))}")
-
-    for file in not_empty_wav_files:
-        kbytes = os.path.getsize(file) / 1024
-        print(f"File - {file}, size - {os.path.getsize(file)/1024} kb")
-
-        current_file = get_file_name(list_of_names)
-        current_size = get_size(current_file)
-
-        if current_size + kbytes < 1024:
+    # 2/3
+    if get_size_in_kb(source_wav) < 1024:
+        list_of_names = [source_wav]
+    else:
+        print("Splitting to small files")
+        not_empty_wav_files = split_wav_by_silence(source_wav)
+        list_of_names = ["generated_audio_file.wav"]
+        with open(list_of_names[0], "wb") as dst_wav_file:
             pass
-        else:
-            print("     ------- LONG FILE")
+        print(f"Start size {get_size_in_kb(list_of_names[-1])}")
 
-            create_new_file_name(list_of_names)
+        # 4
+        for next_small_file in not_empty_wav_files:
+            small_file_size = os.path.getsize(next_small_file) / 1024
 
-            current_file = get_file_name(list_of_names)
-            # current_size = get_size(current_file)
+            print(f"File - {next_small_file}, size - {small_file_size} kb")
+            current_big_file = list_of_names[-1]
+            current_size = get_size_in_kb(current_big_file)
 
-        with open(current_file, "rb") as src_file_wav:
-            src_data = src_file_wav.read()
-        with open(file, "rb") as file_wav:
-            file_data = file_wav.read()
-        with wave.open(current_file, "wb") as dst_wav_file:
-            dst_wav_file.setparams((1, 2, 48000, 0, "NONE", "NONE"))
-            dst_wav_file.writeframes(src_data + file_data)
-        print(f"New size {get_size(current_file)}")
+            if current_size + small_file_size < 1024:
+                pass
+            else:
+                print("     ------- LONG FILE")
+                current_big_file = create_new_file_name(list_of_names)
 
-    print(list_of_names)
+            with open(current_big_file, "rb") as src_file_wav:
+                src_data = src_file_wav.read()
+            with open(next_small_file, "rb") as file_wav:
+                file_data = file_wav.read()
+            with wave.open(current_big_file, "wb") as dst_wav_file:
+                dst_wav_file.setparams((1, 2, 48000, 0, "NONE", "NONE"))
+                dst_wav_file.writeframes(src_data + file_data)
+
+            print(f"New size {get_size_in_kb(current_big_file)}")
+
+        print(list_of_names)
     return list_of_names
 
 
 if __name__ == "__main__":
-    do_the_thing(source_wav="text_from_speach.wav")
+    split_into_files_less_than_1k(source_wav="text_from_speach.wav")
--- yandex_tts.py
+++ yandex_tts.py
@@ -167,7 +167,7 @@ def generate_text_from_speech(source_file):
     result_text = ""
     wav_from_ogg = check_wav_length.convert_from_ogg_to_wav(source_file)
     print("OGG TO WAV - SUCCESS")
-    list_of_audio_files = check_wav_length.do_the_thing(wav_from_ogg)
+    list_of_audio_files = check_wav_length.split_into_files_less_than_1k(wav_from_ogg)
     print("BIG TO SMALL - SUCCESS")
     for file in list_of_audio_files:
         result_text += synthesize_text_from_audio(file)
