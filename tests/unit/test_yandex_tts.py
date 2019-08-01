0bfcad407b75f30a1fc6c2d236e8d864c4253bb8
DanielK <rafikrafikrafik@yandex.ru>
2019-08-01 20:15:29 +0300

    Add first test for yandex_tts module

--- tests/unit/test_yandex_tts.py
+++ tests/unit/test_yandex_tts.py
@@ -2,3 +2,14 @@ import os
 import unittest
 
 from src.yandex_tts import _update_iam_token
+
+
+class TestUpdateIamToken(unittest.TestCase):
+    def test_normal_case(self):
+        """
+        Test normal case 
+        """
+        temp_env_value = "Notarealtoken"
+        os.environ["IAM_TOKEN"] = temp_env_value
+        _update_iam_token()
+        self.assertTrue(os.getenv("IAM_TOKEN")!= temp_env_value)
